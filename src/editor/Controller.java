f0ea6c7bdc65318d693b1b72f9b38a2b989dedc8
Stefan <sheldoncrosss@gmail.com>
2018-04-18 19:19:20 -0400

    Beginning Menu Actions

--- src/editor/Controller.java
+++ src/editor/Controller.java
@@ -1,3 +1,36 @@
 package editor;
 
-public class Controller { }
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private MenuBar mainMenu;
+
+    @FXML
+    private TextField mainText;
+
+    @FXML
+    private TextArea statusBar;
+
+
+    public Controller(){
+
+        //Get save option from main menu
+        MenuItem saveOption = mainMenu.getMenus().get(mainMenu.getMenus().indexOf("Save"));
+
+        //Set the action for the save option
+        saveOption.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });
+    }
+}
--- src/editor/Main.java
+++ src/editor/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Text Editor");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
